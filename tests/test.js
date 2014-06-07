7482be99bc5387dc1c4164da364f5ca5251e2693
ashishbajaj99 <bajaj.ashish@gmail.com>
2014-06-08 01:16:14 +0530

    Added thread affinity support

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,28 @@
 var Thread = require('../index.js');
-var thread = new Thread(3, './compute.js');
+var params = {num: 500000};
+var doneFlag = 15;
 
-var params = {num: 5};
+var thread = new Thread(3, './compute.js', function(out) {
+	console.log("CPU intensive out = ", out);
+	doneFlag--;
+	if(doneFlag == 0) {
+	    thread.close();
+	}
+    });
 
-for(var i = 0; i<10; i++)
-    thread.execute(params);
+thread.execute(params,-1);
 
-console.log("I am done");
+for(var i=0; i<5; i++) {
+    thread.execute({num: 5},1);
+}
+
+for(var i=0; i<5; i++) {
+    thread.execute({num: 5},2);
+}
+
+for(var i=0; i<5; i++) {
+    thread.execute({num: 5},0);
+}
+
+
+console.log("I am done with scheduling");
